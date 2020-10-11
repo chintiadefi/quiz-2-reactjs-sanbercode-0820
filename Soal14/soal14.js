89b13317b87967c48f54f73f4fcd99c76a91c525
chintiadefi <chintiadevi98@gmail.com>
2020-10-11 23:35:17 +0700

    quis pekan 2

--- Soal14/soal14.js
+++ Soal14/soal14.js
@@ -1,3 +1,14 @@
-const volumeBalok = (...) => {
-    return
+const volumeKubus = (sisi) => {
+    return sisi * sisi * sisi
 }
+
+const hasilKubus = volumeKubus(10);
+
+const volumeBalok = (panjang, lebar, tinggi) => {
+    return panjang * lebar * tinggi
+}
+
+const hasilBalok = volumeBalok(10, 4, 3);
+
+const banguRuang = `Volume balok ${hasilBalok}cm dan volume kubus ${hasilKubus}cm`;
+console.log(banguRuang);
